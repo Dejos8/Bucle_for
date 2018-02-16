bae1a8c349a77b24bcbecab8bd721cb8d6d2f142
Dejos8 <35691481+Dejos8@users.noreply.github.com>
2018-02-16 22:38:55 +0100

    Add files via upload

---
+++ for5.py
@@ -0,0 +1,17 @@
+﻿#codiing: utf8
+num = int(input("Cuántos valores vas a introducir"))
+
+if numero <= 0:
+    print("Imposible. Intentalo otra vez")
+else:
+    cont = 0
+    for i in range(1, numero + 1):
+        valor = int(input("Escriba el numero" + str(i) + ":"))
+        if valor < 0:
+            cont = cont + 1
+    if cont == 0:
+        print("No has escrito ningún numero negativo")
+    if cont == 1:
+        print("Has escrito 1 número negativo")
+    else:
+        print"Has escrito", cont "números negativos")
