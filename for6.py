654f74ae5fa7f70e57ed2008acbf103a16ee41d8
Dejos8 <35691481+Dejos8@users.noreply.github.com>
2018-02-16 22:39:13 +0100

    Add files via upload

---
+++ for6.py
@@ -0,0 +1,12 @@
+﻿#coding:utf8
+num = int(input("Cuántos valores vas a introducir?"))
+if num < 1:
+    print("Imposible.Intentalo otra vez")
+else:
+    pares = 0
+    for i in range(1, num + 1):
+        numero = int(input("Escriba el valor + str(i) + ":"))
+        if numero % 2 == 0:
+            pares += 1
+    print "Ha escrito str (pares) numeros pares y" str(num - pares) "números impares")
+ 
